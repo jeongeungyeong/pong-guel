d7cefa8c0e172e5ef60272c722a1df8d27e42d87
jeongeungyeong <jeongeung1124@gmail.com>
2024-09-13 22:51:08 +0900

    docs : 프로젝트 초기 설정

---
+++ src/main/java/org/example/pongguel/PongguelApplication.java
@@ -0,0 +1,13 @@
+package org.example.pongguel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PongguelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PongguelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/pongguel/PongguelApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.pongguel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PongguelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
