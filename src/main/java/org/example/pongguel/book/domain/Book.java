2da7134534bef148d4adc4ed98c0e38b24558bf4
jeongeungyeong <jeongeung1124@gmail.com>
2024-09-17 23:42:03 +0900

    feat: 좋아요 기능을 위한 isLiked 필드 추가

--- src/main/java/org/example/pongguel/book/domain/Book.java
+++ src/main/java/org/example/pongguel/book/domain/Book.java
@@ -3,6 +3,9 @@ package org.example.pongguel.book.domain;
 import jakarta.persistence.*;
 import lombok.*;
 import org.example.pongguel.user.domain.User;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -13,8 +16,8 @@ import org.example.pongguel.user.domain.User;
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(nullable = false)
-    private Long bookID;
+    @Column(name = "book_id", nullable = false)
+    private Long bookId;
 
     @Column(nullable = false)
     private String bookTitle;
@@ -31,7 +34,18 @@ public class Book {
     @Column(nullable = false, unique = true)
     private Long isbn;
 
+    @CreationTimestamp
+    private LocalDate createdAt;
+
+    @Column(name = "is_liked", nullable = false)
+    private boolean isLiked = false;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
+
+    // 좋아요 기능
+    public void toggleLike(){
+        this.isLiked = !this.isLiked;
+    }
 }
