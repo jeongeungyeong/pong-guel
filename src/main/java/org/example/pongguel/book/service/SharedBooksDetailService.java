b70633fef2dbe4bef6e01fdd89831689944f3d53
jeongeungyeong <jeongeung1124@gmail.com>
2024-09-23 02:16:28 +0900

    refactor: 책 공유와 외부인 공유된 책 관련 서비스 분리

--- src/main/java/org/example/pongguel/book/service/SharedBooksDetailService.java
+++ src/main/java/org/example/pongguel/book/service/SharedBooksDetailService.java
@@ -1,9 +1,8 @@
 package org.example.pongguel.book.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.example.pongguel.book.domain.Book;
-import org.example.pongguel.book.dto.BookDetailWithNoteListResponse;
-import org.example.pongguel.book.dto.ShareBookResponse;
 import org.example.pongguel.book.dto.SharedBookDetailWithNoteListResponse;
 import org.example.pongguel.book.repository.BookRepository;
 import org.example.pongguel.exception.ErrorCode;
@@ -12,8 +11,6 @@ import org.example.pongguel.jwt.JwtTokenProvider;
 import org.example.pongguel.note.domain.Note;
 import org.example.pongguel.note.dto.NoteDetailResponse;
 import org.example.pongguel.note.repository.NoteRepository;
-import org.example.pongguel.user.domain.User;
-import org.example.pongguel.user.service.ValidateUser;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,38 +21,14 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 @Transactional
-public class ShareSavedBookService {
+@Slf4j
+public class SharedBooksDetailService {
     @Value("${share_book.share_base_url}")
     private String shareBaseUrl;
 
     private final BookRepository bookRepository;
     private final NoteRepository noteRepository;
-    private final ValidateUser validateUser;
     private final JwtTokenProvider jwtTokenProvider;
-
-
-    // 사용자의 책 공유하기, 공유 토큰 생성
-    public ShareBookResponse shareBook(String token, Long bookId){
-        // 1. 토큰 유효성 검사
-        User user = validateUser.getUserFromToken(token);
-        // 2. bookId로 책 정보 가져오기
-        Book book = bookRepository.findById(bookId)
-                .orElseThrow(()-> new NotFoundException(ErrorCode.BOOK_SAVED_NOT_FOUND));
-        // 3. 공유 토큰 생성하기 (유효기간: 10분)
-        String shareToken = jwtTokenProvider.createShareToken(user.getAccountEmail(),bookId);
-        // 4. 공유 url 생성
-        String shareUrl = createSharedBookUrl(shareToken);
-        // 4. ShareBookResponse 생성 및 반환
-        return new ShareBookResponse(
-                "책이 성공적으로 공유됐습니다.",
-                shareToken,
-                shareUrl,
-                bookId,
-                book.getBookTitle(),
-                user.getNickname(),
-                user.getAccountEmail()
-        );
-    }
 
     // 공유한 책 정보 상세 조회 (외부 접속)
     public SharedBookDetailWithNoteListResponse getSharedBookDetails(String shareToken){
@@ -114,12 +87,6 @@ public class ShareSavedBookService {
                 note.getNoteCreatedAt(),
                 note.isBookmarked()
         );
-    }
-
-    // 공유한 책 노트 상세 조회 (외부 접속)
-    // 공유한 책 상세 url 만들기
-    private String createSharedBookUrl(String token){
-        return String.format("%s/%s",shareBaseUrl,token);
     }
     // 공유한 노트 상세 url 만들기
     private String createNoteBySharedBookUrl(String token,Long noteId){
