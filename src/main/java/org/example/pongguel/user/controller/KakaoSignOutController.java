ce853ea1252988192c8ca799d6991dee0d493063
jeongeungyeong <jeongeung1124@gmail.com>
2024-09-23 03:15:32 +0900

    chore: 안 쓰는 주석 삭제

--- src/main/java/org/example/pongguel/user/controller/KakaoSignOutController.java
+++ src/main/java/org/example/pongguel/user/controller/KakaoSignOutController.java
@@ -24,7 +24,6 @@ public class KakaoSignOutController {
     private final KakaoSignOutService kakaoSignOutService;
     private final JwtUtil jwtUtil;
 
-    // 카카오 로그아웃 페이지로 리다이렉트
     @GetMapping("/sign-out")
     @Operation(summary = "카카오 로그아웃 기능입니다.", description = "서비스 로그아웃뿐만 아니라 카카오톡 액세스토큰도 만료합니다.")
     public ResponseEntity<String> logout(HttpServletRequest request) {
