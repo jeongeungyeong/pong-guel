2be0563a1d32284b604b9b8269aeb2782ac3e4c0
jeongeungyeong <jeongeung1124@gmail.com>
2024-09-23 02:17:07 +0900

    refactor: 분리된 책 공유와 외부인 책 공유 조회 변경사항 반영

--- src/main/java/org/example/pongguel/book/controller/BookShareController.java
+++ src/main/java/org/example/pongguel/book/controller/BookShareController.java
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.example.pongguel.book.dto.SharedBookDetailWithNoteListResponse;
-import org.example.pongguel.book.service.ShareSavedBookService;
+import org.example.pongguel.book.service.SharedBooksDetailService;
 import org.example.pongguel.note.dto.NoteDetailResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,14 +15,14 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @Tag(name="SharedBook",description = "외부인의 공유된 책 관련 서비스 관련된 Api입니다.")
 public class BookShareController {
-    private final ShareSavedBookService shareSavedBookService;
+    private final SharedBooksDetailService sharedBooksDetailService;
 
     // 외부인 책 상세 열람
     @GetMapping("/books/{shareToken}")
     @Operation(summary = "외부인이 공유된 링크로 10분간 책 정보 상세보기", description = "외부인은 퐁글 이용자에게 받은 url로 접속합니다. " +
             " 책 정보와 사용자가 작성한 노트 목록을 볼 수 있습니다.")
     public ResponseEntity<SharedBookDetailWithNoteListResponse> getSharedBookDetails(@PathVariable String shareToken) {
-        SharedBookDetailWithNoteListResponse SharedBookDetailWithNoteListResponse = shareSavedBookService.getSharedBookDetails(shareToken);
+        SharedBookDetailWithNoteListResponse SharedBookDetailWithNoteListResponse = sharedBooksDetailService.getSharedBookDetails(shareToken);
         return ResponseEntity.status(HttpStatus.OK).body(SharedBookDetailWithNoteListResponse);
     }
     // 외부인 노트 상세 열람
@@ -30,7 +30,7 @@ public class BookShareController {
     @Operation(summary = "외부인이 공유된 링크로 10분간 책의 모든 노트 접근하기", description = "외부인은 공유된 책의 노트를 열람할 수 있습니다.")
     public ResponseEntity<NoteDetailResponse> getNotesBySharedBook(@PathVariable("shareToken") String shareToken,
                                                                    @PathVariable("noteId") Long noteId) {
-        NoteDetailResponse noteDetailResponse = shareSavedBookService.getSharedNoteDetail(shareToken,noteId);
+        NoteDetailResponse noteDetailResponse = sharedBooksDetailService.getSharedNoteDetail(shareToken,noteId);
         return ResponseEntity.status(HttpStatus.OK).body(noteDetailResponse);
     }
 }
